db2f8972bac35ce43b445272d834d77e7ef099be
Danchil <84792343+Danchil@users.noreply.github.com>
2022-07-14 23:38:36 +0900

    Update b10926.java

--- JavaStudy_00/src/baekjoon/b10926.java
+++ JavaStudy_00/src/baekjoon/b10926.java
@@ -1,19 +1,13 @@
 package baekjoon;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class b10926 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		ArrayList<String> idList = new ArrayList<>();
 		String id = in.next();
-		idList.add(id);
-
-		if (idList.get(0) == id) {
-			System.out.print(id + "??!");
-		}
+		System.out.print(id + "??!");
 
 	}
 
